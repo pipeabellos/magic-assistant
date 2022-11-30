58f338c304a37992969c4d65088b0e7833d75405
Felipe Abello <pipe@helloguru.io>
2022-11-30 16:35:05 -0500

    latest build

---
+++ pages/api/generate.js
@@ -0,0 +1,27 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = `Write me a Linkedin post in the style of Paul Graham with the title below. Please write it in first person. Try to be a little contrarian and also assertive.
+
+                            Title:`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 100,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
